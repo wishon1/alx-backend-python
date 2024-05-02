95d4c6f78383f1ae0513f05dbdd0b494c3bd63dd
wishon1 <wisdomhonest1@gmail.com>
2024-05-02 17:21:53 -0400

    add task 4

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Basic annotations - to_str:
+    Type-annotated function to_str that takes a float n as argument
+    and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    to_str: Type-annotated function that takes a float n as argument
+    and returns the string representation of the float.
+
+    Args:
+        n (float): A floating-point number.
+
+    Returns:
+        str: The string representation of the float.
+    """
+    return str(n)
---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Basic annotations - define variables:
+    Defines and annotates variables with specified values.
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
