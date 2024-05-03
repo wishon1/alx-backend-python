df61f2b84296ba102a2193a5fc1a381d1342dce1
wishon1 <wisdomhonest1@gmail.com>
2024-05-03 14:30:43 -0400

    add update to 102

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -20,7 +20,7 @@ zoom_3x = zoom_array(array, 3.0)
 from typing import Tuple, List, Any
 
 
-def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> List[Any]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
     zoom_array: Function to zoom in an array by repeating each element
     a given number of times.
@@ -39,8 +39,10 @@ def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> List[Any]:
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3)
+zoom_3x =
+
+zoom_array(array, 3)
