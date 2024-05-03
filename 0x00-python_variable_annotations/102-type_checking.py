e5ca83d28128ec5a06d587bddcb2fac1b396883d
wishon1 <wisdomhonest1@gmail.com>
2024-05-03 13:31:13 -0400

    add 102

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+Use mypy to validate the following piece of code and apply any
+necessary changes.
+
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    zoomed_in: Tuple = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
+"""
+from typing import Tuple, List, Any
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    zoom_array: Function to zoom in an array by repeating each element
+    a given number of times.
+
+    Args:
+        lst: A tuple containing elements to be zoomed in.
+        factor: An integer representing the zoom factor. Defaults to 2.
+
+    Returns:
+        A list containing the zoomed-in elements.
+    """
+    zoomed_in: Tuple = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
